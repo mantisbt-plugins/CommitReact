87ceeb4b0058332e6fbedf1bdf8534d35eb748ba
spmeesseman <spmeesseman@gmail.com>
2019-08-03 01:02:24 -0400

    featmin: show the success redirect when saving config settings

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -1,5 +1,6 @@
 <?php
 
+form_security_validate( 'plugin_CommitReact_config_edit' );
 auth_reauthenticate();
 
 access_ensure_global_level(config_get('manage_plugin_threshold'));
@@ -10,4 +11,11 @@ plugin_config_set('unattach_tags', gpc_get_string('unattach_tags', ''));
 plugin_config_set('resolved_attach_tags', gpc_get_string('resolved_attach_tags', ''));
 plugin_config_set('resolved_unattach_tags', gpc_get_string('resolved_unattach_tags', ''));
 
-print_successful_redirect(plugin_page('config', TRUE));
+form_security_purge( 'plugin_CommitReact_config_edit' );
+
+$t_redirect_url = plugin_page('config', TRUE);
+
+layout_page_header( null, $t_redirect_url );
+layout_page_begin();
+html_operation_successful( $t_redirect_url );
+layout_page_end();
